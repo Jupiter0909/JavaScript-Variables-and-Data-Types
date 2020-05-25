5f6b9d96c77f1ed732729638980c9e6b38032d3e
Jupiter0909 <61379653+Jupiter0909@users.noreply.github.com>
2020-05-25 17:16:31 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,79 @@
+// Console log
+console.log("Codinger");
+
+// Variables
+
+// String
+var name = "codinger ";
+var lastName = "blade";
+console.log(name);
+console.log(typeof name);
+
+// Integer & float
+var num = 5;
+var num2 = 5.5;
+console.log(num);
+console.log(typeof num);
+console.log(typeof num2);
+
+// Null
+var smth = null;
+var smth2 = null;
+console.log(smth);
+console.log(typeof smth);
+
+// Undefined
+var nth = undefined;
+var nth2 = undefined;
+console.log(nth);
+console.log(typeof nth);
+
+// Boolean
+var b = true;
+var a = false;
+console.log(a);
+console.log(b);
+console.log(typeof a);
+console.log(typeof b);
+
+// Data Type Operations
+var fullName = name + lastName;
+console.log(fullName);
+console.log(typeof fullName);
+
+var total = num + num2;
+console.log(total);
+console.log(typeof total);
+
+var something = smth + smth2;
+console.log(something);
+console.log(typeof something);
+
+var nothing = nth + nth2;
+console.log(nothing);
+console.log(typeof nothing);
+
+var bool = a + b;
+console.log(bool);
+console.log(typeof bool);
+
+// Function
+var frame = function () {
+  console.log("name");
+};
+
+frame();
+console.log(typeof frame);
+// Array
+var arr = [1, 2, 3, 4, 5];
+console.log(arr);
+console.log(typeof arr);
+
+var full = { firstName: "Codinger", lastName: "Blade" };
+console.log(full);
+console.log(typeof full);
+
+// Three variable types
+const Const = 9;
+let Let = 3;
+var Var = 0;
